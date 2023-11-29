17f3695eb3ec5c8c39b21bcc17feb306cb2763a0
deckxter <abantoq@gmail.com>
2023-11-29 01:15:25 -0500

    First commit, initial dependencies added

---
+++ src/test/java/com/demo/smartjob/SmartjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.smartjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
