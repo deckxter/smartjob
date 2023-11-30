ec2ef941c7a6522469581f9e80d58a1ab1eb6ef2
deckxter <abantoq@gmail.com>
2023-11-30 15:52:50 -0500

    Adding postman collection to test the user api

--- src/main/java/com/demo/smartjob/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/demo/smartjob/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.demo.smartjob.exceptions;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,9 +14,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {ResourceNotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorDetails resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        System.out.println("Catchinc ResourceNotFoundException");
+    public ErrorDetails resourceNotFoundException(ResourceNotFoundException ex) {
         return new ErrorDetails(KEY_MESSAGE, ex.getMessage());
     }
 
+    //TODO Pending to manage constraint violation exception for email
+    @ExceptionHandler(value = {})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDetails constraintViolationException(ConstraintViolationException ex) {
+        StringBuilder errorBuffer = new StringBuilder("Error in these constraints: ");
+        for (ConstraintViolation violation : ex.getConstraintViolations()) {
+            errorBuffer.append(violation.getMessage());
+        }
+        return new ErrorDetails(KEY_MESSAGE, errorBuffer.toString());
+    }
 }
